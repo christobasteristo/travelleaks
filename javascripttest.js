7f166d0ccbc918422083ca7dbb673a9272fb5a7f
christobasteristo <christopherbastert@gmail.com>
2018-08-01 12:51:21 +0200

    ready to deploy

--- javascripttest.js
+++ javascripttest.js
@@ -1,9 +1,24 @@
 
-alert("Hey your interested? Alright...Take a deep breath and click OK ;)!");
+alert("Hey mate! This page is currently under construction...But lean back - it'll very soon rock your butt!");
 
+window.onload = function() {
 
+  var n = localStorage.getItem('on_load_counter');
 
+  if (n === null) {
+    n = 0;
+  }
+  n++;
 
+  localStorage.setItem("on_load_counter", n);
+
+  nums = n.toString().split('').map(Number);
+  document.getElementById('CounterVisitor').innerHTML = '';
+  for (var i of nums) {
+    document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
+  }
+
+};
 
 
 console.log('Yes it is');
